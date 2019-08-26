98d4fe9a98ae95679d88c44cb2ba27b9a7e110be
Harbinder <harbinder03@gmail.com>
2019-08-26 18:53:23 +0800

    4. Services

--- tourofheroes/src/app/heroes/heroes.component.ts
+++ tourofheroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 
 @Component({
@@ -10,17 +9,21 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
   
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(public heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   selectedHero: Hero;
   onSelect(hero:Hero) {
     this.selectedHero = hero;
-
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
